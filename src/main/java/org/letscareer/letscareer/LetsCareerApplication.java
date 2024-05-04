15b1ba04f0bf0ea131bb4aeec5ea0e5be05fa4f0
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-04 17:00:58 +0900

    [init] 프로그램 초기 설정

---
+++ src/main/java/org/letscareer/letscareer/LetsCareerApplication.java
@@ -0,0 +1,13 @@
+package org.letscareer.letscareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsCareerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsCareerApplication.class, args);
+    }
+
+}
