5fed994adf461574c5718c83e1f0151f867b7c3e
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-05 21:42:05 +0900

    [feat] #4 application entity 생성

---
+++ src/main/java/org/letscareer/letscareer/domain/application/entity/Application.java
@@ -0,0 +1,28 @@
+package org.letscareer.letscareer.domain.application.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.letscareer.letscareer.domain.price.entity.UserPayment;
+import org.letscareer.letscareer.global.common.entity.BaseTimeEntity;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING)
+@Getter
+@Entity
+public abstract class Application extends BaseTimeEntity {
+    @Id
+    @Column(name = "application_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_payment_id")
+    private UserPayment userPayment;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User user;
+}
