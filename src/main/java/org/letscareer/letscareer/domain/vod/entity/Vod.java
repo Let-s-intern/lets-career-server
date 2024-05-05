6df909c9fa517558d833ac7c1e02ae4aaa041450
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-05 21:41:01 +0900

    [feat] #4 vod entity 생성

---
+++ src/main/java/org/letscareer/letscareer/domain/vod/entity/Vod.java
@@ -0,0 +1,30 @@
+package org.letscareer.letscareer.domain.vod.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.letscareer.letscareer.domain.classification.entity.VodClassification;
+import org.letscareer.letscareer.global.common.entity.BaseTimeEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "vod")
+@Entity
+public class Vod extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vod_id")
+    private Long id;
+    private String title;
+    private String shortDesc;
+    private String thumbnail;
+    private String stringJob;
+
+    @OneToMany(mappedBy = "vod", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<VodClassification> classificationList = new ArrayList<>();
+}
