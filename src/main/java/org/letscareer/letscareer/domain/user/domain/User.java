f4c80ec44ecf148032810bdcf551a02bc8d6a51e
seoyamin <seoyamin@ewhain.net>
2024-05-05 11:50:36 +0900

    [init] #3 create User domain

---
+++ src/main/java/org/letscareer/letscareer/domain/user/domain/User.java
@@ -0,0 +1,57 @@
+package org.letscareer.letscareer.domain.user.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.letscareer.letscareer.global.security.oauth2.AuthProvider;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String email;
+
+    @Nullable
+    private String contactEmail;
+
+    @Nullable
+    @JsonIgnore
+    private String password;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String phoneNum;
+
+    @Nullable
+    private String university;
+
+    @Nullable
+    private String major;
+
+    @Nullable
+    private AccountType accountType;
+
+    @Nullable
+    private String accountNum;
+
+    @Nullable
+    private AuthProvider authProvider;
+
+    @NotNull
+    @Builder.Default
+    private UserRole role = UserRole.USER;
+
+}
---
+++ src/main/java/org/letscareer/letscareer/domain/user/domain/UserRole.java
@@ -0,0 +1,16 @@
+package org.letscareer.letscareer.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.letscareer.letscareer.global.common.utils.EnumField;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum UserRole implements EnumField {
+    ADMIN(1, "관리자"),
+    USER(2, "사용자");
+
+    private final Integer code;
+    private final String desc;
+}
