b11a8c6beeeabf2c5f1d10f097e7806d0dbe490d
seoyamin <seoyamin@ewhain.net>
2024-05-05 13:00:39 +0900

    [init] #3 create Jwt TokenProvider, Handler, Filter, EntryPoint

---
+++ src/main/java/org/letscareer/letscareer/global/security/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,41 @@
+package org.letscareer.letscareer.global.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.letscareer.letscareer.global.error.GlobalErrorCode;
+import org.letscareer.letscareer.global.error.entity.ErrorResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static org.letscareer.letscareer.global.error.GlobalErrorCode.FORBIDDEN;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        responseToClient(response, getErrorResponse(FORBIDDEN));
+    }
+
+    private ErrorResponse getErrorResponse(GlobalErrorCode globalErrorCode) {
+        return ErrorResponse.of(globalErrorCode);
+    }
+
+    private void responseToClient(HttpServletResponse response, ErrorResponse errorResponse) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(errorResponse.getStatus());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
