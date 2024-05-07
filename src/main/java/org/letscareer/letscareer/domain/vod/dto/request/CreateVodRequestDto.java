ae034fd92c446cf28c3d5a918628709ffd786b7f
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-08 00:25:52 +0900

    [feat] #10 vod 생성 dto 구현

---
+++ src/main/java/org/letscareer/letscareer/domain/vod/dto/request/CreateVodRequestDto.java
@@ -0,0 +1,14 @@
+package org.letscareer.letscareer.domain.vod.dto.request;
+
+import org.letscareer.letscareer.domain.classification.dto.request.CreateVodClassificationRequestDto;
+
+import java.util.List;
+
+public record CreateVodRequestDto(
+        String title,
+        String shortDesc,
+        String thumbnail,
+        String stringJob,
+        List<CreateVodClassificationRequestDto> programTypeInfo
+) {
+}
