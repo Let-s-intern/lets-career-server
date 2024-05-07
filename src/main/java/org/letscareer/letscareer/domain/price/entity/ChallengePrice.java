0b61084c55a797ec8e0b89c8d0c5d656a5d90207
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-08 00:23:55 +0900

    [feat] #10 challenge price 생성 로지 구현

--- src/main/java/org/letscareer/letscareer/domain/price/entity/ChallengePrice.java
+++ src/main/java/org/letscareer/letscareer/domain/price/entity/ChallengePrice.java
@@ -4,14 +4,15 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.letscareer.letscareer.domain.challenge.entity.Challenge;
 import org.letscareer.letscareer.domain.challenge.type.converter.ChallengeTypeConverter;
+import org.letscareer.letscareer.domain.price.dto.request.CreateChallengePriceRequestDto;
 import org.letscareer.letscareer.domain.price.type.ChallengeParticipationType;
 import org.letscareer.letscareer.domain.price.type.ChallengePriceType;
 import org.letscareer.letscareer.domain.price.type.ChallengeUserType;
+import org.letscareer.letscareer.domain.price.type.converter.ChallengeParticipationTypeConverter;
 import org.letscareer.letscareer.domain.price.type.converter.ChallengePriceTypeConverter;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder(access = AccessLevel.PRIVATE)
 @DiscriminatorValue("challenge_price")
 @Getter
 @Entity
@@ -22,9 +23,27 @@ public class ChallengePrice extends Price {
     private ChallengePriceType challengePriceType;
     @Convert(converter = ChallengeTypeConverter.class)
     private ChallengeUserType challengeUserType;
-    @Convert(converter = ChallengeParticipationType.class)
+    @Convert(converter = ChallengeParticipationTypeConverter.class)
     private ChallengeParticipationType challengeParticipationType;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "challenge_id")
     private Challenge challenge;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    public ChallengePrice(CreateChallengePriceRequestDto requestDto,
+                          Challenge challenge) {
+        super(requestDto.priceInfo());
+        this.challengePriceType = requestDto.challengePriceType();
+        this.challengeUserType = requestDto.challengeUserType();
+        this.challengeParticipationType = requestDto.challengeParticipationType();
+        challenge.addChallengePriceList(this);
+    }
+
+    public static ChallengePrice createChallengePrice(CreateChallengePriceRequestDto requestDto,
+                                                      Challenge challenge) {
+        return ChallengePrice.builder()
+                .requestDto(requestDto)
+                .challenge(challenge)
+                .build();
+    }
 }
