979fcebf987390d4f6d14b2cef8d9c17516422d8
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-08 00:28:52 +0900

    [feat] #10 price entity Builder Default 취소

--- src/main/java/org/letscareer/letscareer/domain/price/entity/Price.java
+++ src/main/java/org/letscareer/letscareer/domain/price/entity/Price.java
@@ -2,10 +2,9 @@ package org.letscareer.letscareer.domain.price.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.letscareer.letscareer.domain.price.dto.request.CreatePriceRequestDto;
 import org.letscareer.letscareer.domain.price.type.AccountType;
-import org.letscareer.letscareer.domain.price.type.ChallengePriceType;
 import org.letscareer.letscareer.domain.price.type.converter.AccountTypeConverter;
-import org.letscareer.letscareer.domain.price.type.converter.ChallengePriceTypeConverter;
 import org.letscareer.letscareer.global.common.entity.BaseTimeEntity;
 
 import java.time.LocalDateTime;
@@ -21,12 +20,18 @@ public abstract class Price extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "price_id")
     private Long id;
-    @Builder.Default
     private Integer price = 0;
-    @Builder.Default
     private Integer discount = 0;
     private String accountNumber;
     private LocalDateTime deadline;
     @Convert(converter = AccountTypeConverter.class)
     private AccountType accountType;
+
+    public Price(CreatePriceRequestDto requestDto) {
+        this.price = requestDto.price();
+        this.discount = requestDto.discount();
+        this.accountNumber = requestDto.accountNumber();
+        this.deadline = requestDto.deadline();
+        this.accountType = requestDto.accountType();
+    }
 }
