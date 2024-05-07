27ee9cc58610b18939cd37e59fa234aa3b58dec4
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-08 00:28:30 +0900

    [feat] #10 vod & vod classification & vod price 생성 로직 구현

---
+++ src/main/java/org/letscareer/letscareer/domain/classification/helper/VodClassificationHelper.java
@@ -0,0 +1,22 @@
+package org.letscareer.letscareer.domain.classification.helper;
+
+import lombok.RequiredArgsConstructor;
+import org.letscareer.letscareer.domain.classification.dto.request.CreateVodClassificationRequestDto;
+import org.letscareer.letscareer.domain.classification.entity.VodClassification;
+import org.letscareer.letscareer.domain.classification.repository.VodClassificationRepository;
+import org.letscareer.letscareer.domain.vod.entity.Vod;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Component
+public class VodClassificationHelper {
+    private final VodClassificationRepository vodClassificationRepository;
+
+    public VodClassification createVodClassificationAndSave(CreateVodClassificationRequestDto requestDto,
+                                                            Vod vod) {
+        VodClassification vodClassification = VodClassification.createVodClassification(requestDto, vod);
+        return vodClassificationRepository.save(vodClassification);
+    }
+}
