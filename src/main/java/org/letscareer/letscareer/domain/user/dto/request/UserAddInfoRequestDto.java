5100792f2808ebcbfabb0193273b38d7c86f0d65
seoyamin <seoyamin@ewhain.net>
2024-05-13 11:12:24 +0900

    [fix] #28 User 추가 정보 입력 contactEmail 추가

--- src/main/java/org/letscareer/letscareer/domain/user/dto/request/UserAddInfoRequestDto.java
+++ src/main/java/org/letscareer/letscareer/domain/user/dto/request/UserAddInfoRequestDto.java
@@ -5,11 +5,12 @@ import org.letscareer.letscareer.domain.user.type.UserGrade;
 
 public record UserAddInfoRequestDto(
         String university,
-        String major,
         UserGrade grade,
+        String major,
         String wishJob,
         String wishCompany,
         Boolean marketingAgree,
+        String contactEmail,
         AccountType accountType,
         String accountNum,
         String accountOwner
