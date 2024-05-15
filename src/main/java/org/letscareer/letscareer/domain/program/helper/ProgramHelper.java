9243a7c92b8066a2116e0c71d9d0c5944d196d2c
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-15 19:47:43 +0900

    [feat] #32 repository dependency helper 구현

---
+++ src/main/java/org/letscareer/letscareer/domain/program/helper/ProgramHelper.java
@@ -0,0 +1,20 @@
+package org.letscareer.letscareer.domain.program.helper;
+
+import lombok.RequiredArgsConstructor;
+import org.letscareer.letscareer.domain.program.repository.ProgramRepository;
+import org.letscareer.letscareer.domain.program.vo.AdminProgramVo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Component
+public class ProgramHelper {
+    private final ProgramRepository programRepository;
+
+    public Page<AdminProgramVo> findAdminProgramVos(Pageable pageable) {
+        return programRepository.findAdminProgramVos(pageable);
+    }
+}
