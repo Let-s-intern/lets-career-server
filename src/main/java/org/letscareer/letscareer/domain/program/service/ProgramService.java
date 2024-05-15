61329d60b87138ca59815e34faeabd8659a2b887
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-15 19:46:48 +0900

    [refactor] #32 불필요한 import 제거

--- src/main/java/org/letscareer/letscareer/domain/program/service/ProgramService.java
+++ src/main/java/org/letscareer/letscareer/domain/program/service/ProgramService.java
@@ -1,9 +1,9 @@
 package org.letscareer.letscareer.domain.program.service;
 
 import org.letscareer.letscareer.domain.program.dto.response.GetProgramsResponseDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 
 @Service
 public interface ProgramService {
