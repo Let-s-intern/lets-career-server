a44f9b046550921598d3d4e19bcca6449a8824ca
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-16 01:13:30 +0900

    [feat] #39 faq crud query 구현

--- src/main/java/org/letscareer/letscareer/domain/faq/repository/FaqQueryRepository.java
+++ src/main/java/org/letscareer/letscareer/domain/faq/repository/FaqQueryRepository.java
@@ -1,10 +1,14 @@
 package org.letscareer.letscareer.domain.faq.repository;
 
+import org.letscareer.letscareer.domain.faq.type.FaqProgramType;
 import org.letscareer.letscareer.domain.faq.vo.FaqDetailVo;
 
 import java.util.List;
 
 public interface FaqQueryRepository {
     List<FaqDetailVo> findChallengeFaqDetailVos(Long challengeId);
+
     List<FaqDetailVo> findLiveFaqDetailVos(Long liveId);
+
+    List<FaqDetailVo> findFaqDetailVosForType(FaqProgramType type);
 }
