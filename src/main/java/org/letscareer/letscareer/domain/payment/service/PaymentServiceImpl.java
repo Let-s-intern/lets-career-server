294289f0a13712aa45e486b57e762434410a9dc2
seoyamin <seoyamin@ewhain.net>
2024-05-18 02:46:02 +0900

    [feat] #41 add Payment Update API Swagger Docs

--- src/main/java/org/letscareer/letscareer/domain/payment/service/PaymentServiceImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/payment/service/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package org.letscareer.letscareer.domain.payment.service;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.letscareer.letscareer.domain.payment.dto.request.UpdatePaymentRequestDto;
 import org.letscareer.letscareer.domain.payment.entity.Payment;
@@ -13,6 +15,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentHelper paymentHelper;
 
+    @Operation(summary = "결제 내역 업데이트", responses = {
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true)
+    })
     @Override
     public void updatePayment(Long paymentId, UpdatePaymentRequestDto updatePaymentRequestDto) {
         Payment payment = paymentHelper.findPaymentByIdOrThrow(paymentId);
