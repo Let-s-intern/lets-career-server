a9edc90e97133e8cde5d07f293f8f0790856b47a
seoyamin <seoyamin@ewhain.net>
2024-05-18 02:41:33 +0900

    [feat] #41 create Payment Update API

---
+++ src/main/java/org/letscareer/letscareer/domain/payment/error/PaymentErrorCode.java
@@ -0,0 +1,16 @@
+package org.letscareer.letscareer.domain.payment.error;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.letscareer.letscareer.global.error.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum PaymentErrorCode implements ErrorCode {
+    PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 결제 내역입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
--- src/main/java/org/letscareer/letscareer/domain/payment/service/PaymentServiceImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/payment/service/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package org.letscareer.letscareer.domain.payment.service;
 
 import lombok.RequiredArgsConstructor;
+import org.letscareer.letscareer.domain.payment.dto.request.UpdatePaymentRequestDto;
+import org.letscareer.letscareer.domain.payment.entity.Payment;
 import org.letscareer.letscareer.domain.payment.helper.PaymentHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentHelper paymentHelper;
+
+    @Override
+    public void updatePayment(Long paymentId, UpdatePaymentRequestDto updatePaymentRequestDto) {
+        Payment payment = paymentHelper.findPaymentByIdOrThrow(paymentId);
+        payment.updatePayment(updatePaymentRequestDto);
+    }
 }
