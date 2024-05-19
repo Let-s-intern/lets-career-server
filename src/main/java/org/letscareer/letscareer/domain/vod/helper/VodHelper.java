f57b3107f76f202a2515a92a90ac83807035c7d3
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-19 17:28:06 +0900

    [feat] #42 vod 목록 조회 쿼리 호출 메서드 구현

--- src/main/java/org/letscareer/letscareer/domain/vod/helper/VodHelper.java
+++ src/main/java/org/letscareer/letscareer/domain/vod/helper/VodHelper.java
@@ -1,11 +1,15 @@
 package org.letscareer.letscareer.domain.vod.helper;
 
 import lombok.RequiredArgsConstructor;
+import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
 import org.letscareer.letscareer.domain.vod.dto.request.CreateVodRequestDto;
 import org.letscareer.letscareer.domain.vod.entity.Vod;
 import org.letscareer.letscareer.domain.vod.repository.VodRepository;
 import org.letscareer.letscareer.domain.vod.vo.VodDetailVo;
+import org.letscareer.letscareer.domain.vod.vo.VodProfileVo;
 import org.letscareer.letscareer.global.error.exception.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,6 +34,10 @@ public class VodHelper {
     public Vod findVodByIdOrThrow(Long vodId) {
         return vodRepository.findById(vodId)
                 .orElseThrow(() -> new EntityNotFoundException(VOD_NOT_FOUND));
+    }
+
+    public Page<VodProfileVo> findVodProfileVos(ProgramClassification type, Pageable pageable) {
+        return vodRepository.findVodProfileVos(type, pageable);
     }
 
     public void deleteVodById(Long vodId) {
