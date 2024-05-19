81f4a4f9243995c0a60118c4174dcbadf0bc50ba
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-19 16:44:23 +0900

    [feat] #42 live profile 조회 비즈니스 로직 구현

--- src/main/java/org/letscareer/letscareer/domain/live/service/LiveService.java
+++ src/main/java/org/letscareer/letscareer/domain/live/service/LiveService.java
@@ -1,13 +1,18 @@
 package org.letscareer.letscareer.domain.live.service;
 
 import org.letscareer.letscareer.domain.application.dto.response.GetLiveApplicationsResponseDto;
+import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
 import org.letscareer.letscareer.domain.live.dto.request.CreateLiveRequestDto;
 import org.letscareer.letscareer.domain.live.dto.response.GetLiveDetailResponseDto;
+import org.letscareer.letscareer.domain.live.dto.response.GetLivesResponseDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public interface LiveService {
+    GetLivesResponseDto getLiveList(ProgramClassification type, Pageable pageable);
+
     GetLiveDetailResponseDto getLiveDetail(Long liveId);
 
     GetLiveApplicationsResponseDto getApplications(Long liveId, Boolean isConfirmed);
