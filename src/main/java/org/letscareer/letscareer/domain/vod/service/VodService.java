6215ddca6942367d5a8bf4c30f9db39180e53711
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-19 17:28:53 +0900

    [feat] #42 vod 목록 조회 비즈니스 로직 구현

--- src/main/java/org/letscareer/letscareer/domain/vod/service/VodService.java
+++ src/main/java/org/letscareer/letscareer/domain/vod/service/VodService.java
@@ -1,11 +1,15 @@
 package org.letscareer.letscareer.domain.vod.service;
 
+import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
 import org.letscareer.letscareer.domain.vod.dto.request.CreateVodRequestDto;
 import org.letscareer.letscareer.domain.vod.dto.response.GetVodDetailResponseDto;
+import org.letscareer.letscareer.domain.vod.dto.response.GetVodsResponseDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface VodService {
+    GetVodsResponseDto getVodList(ProgramClassification type, Pageable pageable);
     GetVodDetailResponseDto getVodDetail(Long vodId);
     void createVod(CreateVodRequestDto createVodRequestDto);
     void updateVod(Long vodId, CreateVodRequestDto createVodRequestDto);
--- src/main/java/org/letscareer/letscareer/domain/vod/service/VodServiceImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/vod/service/VodServiceImpl.java
@@ -3,13 +3,18 @@ package org.letscareer.letscareer.domain.vod.service;
 import lombok.RequiredArgsConstructor;
 import org.letscareer.letscareer.domain.classification.dto.request.CreateVodClassificationRequestDto;
 import org.letscareer.letscareer.domain.classification.helper.VodClassificationHelper;
+import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
 import org.letscareer.letscareer.domain.classification.vo.VodClassificationDetailVo;
 import org.letscareer.letscareer.domain.vod.dto.request.CreateVodRequestDto;
 import org.letscareer.letscareer.domain.vod.dto.response.GetVodDetailResponseDto;
+import org.letscareer.letscareer.domain.vod.dto.response.GetVodsResponseDto;
 import org.letscareer.letscareer.domain.vod.entity.Vod;
 import org.letscareer.letscareer.domain.vod.helper.VodHelper;
 import org.letscareer.letscareer.domain.vod.mapper.VodMapper;
 import org.letscareer.letscareer.domain.vod.vo.VodDetailVo;
+import org.letscareer.letscareer.domain.vod.vo.VodProfileVo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,10 +29,16 @@ public class VodServiceImpl implements VodService {
     private final VodClassificationHelper vodClassificationHelper;
 
     @Override
+    public GetVodsResponseDto getVodList(ProgramClassification type, Pageable pageable) {
+        Page<VodProfileVo> vodProfileVos = vodHelper.findVodProfileVos(type, pageable);
+        return vodMapper.toGetVodsResponseDto(vodProfileVos);
+    }
+
+    @Override
     public GetVodDetailResponseDto getVodDetail(Long vodId) {
         VodDetailVo vodInfo = vodHelper.findVodDetailVoOrThrow(vodId);
         List<VodClassificationDetailVo> programTypeInfo = vodClassificationHelper.findVodClassificationVos(vodId);
-        return vodMapper.createVodDetailResponseDto(vodInfo, programTypeInfo);
+        return vodMapper.toVodDetailResponseDto(vodInfo, programTypeInfo);
     }
 
     @Override
