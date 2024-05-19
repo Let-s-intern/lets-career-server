0b1475b9e7f5e91270b2a8c1f50d27826ad64249
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-19 17:27:17 +0900

    [feat] #42 vod 목록 조회 response dto 구현

---
+++ src/main/java/org/letscareer/letscareer/domain/vod/dto/response/GetVodsResponseDto.java
@@ -0,0 +1,23 @@
+package org.letscareer.letscareer.domain.vod.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.letscareer.letscareer.domain.vod.vo.VodProfileVo;
+import org.letscareer.letscareer.global.common.entity.PageInfo;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record GetVodsResponseDto(
+        List<VodProfileVo> progrmList,
+        PageInfo pageInfo
+) {
+    public static GetVodsResponseDto of(Page<VodProfileVo> programList) {
+        PageInfo pageInfo = PageInfo.of(programList);
+        return GetVodsResponseDto.builder()
+                .progrmList(programList.getContent())
+                .pageInfo(pageInfo)
+                .build();
+    }
+}
