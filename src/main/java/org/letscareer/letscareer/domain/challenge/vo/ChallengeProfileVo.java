95bbd40fae9915ee072490caabd12ad20a739054
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-19 15:43:25 +0900

    [feat] #42 challenge profile 조회 vo 구현

---
+++ src/main/java/org/letscareer/letscareer/domain/challenge/vo/ChallengeProfileVo.java
@@ -0,0 +1,17 @@
+package org.letscareer.letscareer.domain.challenge.vo;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record ChallengeProfileVo(
+        Long id,
+        String title,
+        String shortDesc,
+        String thumbnail,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        LocalDateTime deadline
+) {
+}
