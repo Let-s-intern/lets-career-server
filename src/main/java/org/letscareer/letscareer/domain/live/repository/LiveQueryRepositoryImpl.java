a7bdb9b938c90effa0d34166906c33f2f108fcd0
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-19 16:44:13 +0900

    [feat] #42 live profile 조회 쿼리 구현

--- src/main/java/org/letscareer/letscareer/domain/live/repository/LiveQueryRepositoryImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/live/repository/LiveQueryRepositoryImpl.java
@@ -2,12 +2,21 @@ package org.letscareer.letscareer.domain.live.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
+import org.letscareer.letscareer.domain.live.entity.Live;
 import org.letscareer.letscareer.domain.live.vo.LiveDetailVo;
+import org.letscareer.letscareer.domain.live.vo.LiveProfileVo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.letscareer.letscareer.domain.classification.entity.QLiveClassification.liveClassification;
 import static org.letscareer.letscareer.domain.live.entity.QLive.live;
 
 @RequiredArgsConstructor
@@ -18,6 +27,7 @@ public class LiveQueryRepositoryImpl implements LiveQueryRepository {
     public Optional<LiveDetailVo> findLiveDetailVo(Long liveId) {
         return Optional.ofNullable(jpaQueryFactory
                 .select(Projections.constructor(LiveDetailVo.class,
+                        live.id,
                         live.title,
                         live.shortDesc,
                         live.description,
@@ -38,7 +48,42 @@ public class LiveQueryRepositoryImpl implements LiveQueryRepository {
         );
     }
 
+    @Override
+    public Page<LiveProfileVo> findLiveProfileVos(ProgramClassification type, Pageable pageable) {
+        List<LiveProfileVo> contents = jpaQueryFactory
+                .select(Projections.constructor(LiveProfileVo.class,
+                        live.id,
+                        live.title,
+                        live.shortDesc,
+                        live.thumbnail,
+                        live.startDate,
+                        live.endDate,
+                        live.deadline
+                ))
+                .from(live)
+                .leftJoin(
+                        live.classificationList, liveClassification
+                )
+                .where(
+                        eqLiveClassification(type)
+                )
+                .orderBy()
+                .fetch();
+
+        JPAQuery<Live> countQuery = jpaQueryFactory
+                .selectFrom(live)
+                .where(
+                        eqLiveClassification(type)
+                );
+
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchCount);
+    }
+
     private BooleanExpression eqLiveId(Long liveId) {
         return liveId != null ? live.id.eq(liveId) : null;
+    }
+
+    private BooleanExpression eqLiveClassification(ProgramClassification type) {
+        return type != null ? liveClassification.programClassification.eq(type) : null;
     }
 }
