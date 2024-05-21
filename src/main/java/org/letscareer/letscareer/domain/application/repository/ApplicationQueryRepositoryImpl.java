baae45ee7d095c0aa9a178a208620604f634d086
seoyamin <seoyamin@ewhain.net>
2024-05-21 17:09:14 +0900

    [feat] #41 create My Application List API

--- src/main/java/org/letscareer/letscareer/domain/application/repository/ApplicationQueryRepositoryImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/application/repository/ApplicationQueryRepositoryImpl.java
@@ -1,10 +1,14 @@
 package org.letscareer.letscareer.domain.application.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.letscareer.letscareer.domain.application.vo.MyApplicationVo;
 
 import java.util.List;
+
+import static org.letscareer.letscareer.domain.application.entity.QVWApplication.vWApplication;
 
 @RequiredArgsConstructor
 public class ApplicationQueryRepositoryImpl implements ApplicationQueryRepository {
@@ -12,6 +16,20 @@ public class ApplicationQueryRepositoryImpl implements ApplicationQueryRepositor
 
     @Override
     public List<MyApplicationVo> findMyApplications(Long userId) {
-        return null;
+        return queryFactory.select(Projections.constructor(MyApplicationVo.class,
+                        vWApplication.applicationId,
+                        vWApplication.paymentIsConfirmed,
+                        vWApplication.programTitle,
+                        vWApplication.programStartDate,
+                        vWApplication.programEndDate))
+                .from(vWApplication)
+                .where(
+                        eqUserId(userId)
+                )
+                .fetch();
+    }
+
+    private BooleanExpression eqUserId(Long userId) {
+        return vWApplication.userId.eq(userId);
     }
 }
--- src/main/java/org/letscareer/letscareer/domain/application/vo/MyApplicationVo.java
+++ src/main/java/org/letscareer/letscareer/domain/application/vo/MyApplicationVo.java
@@ -1,12 +1,23 @@
 package org.letscareer.letscareer.domain.application.vo;
 
 import lombok.Builder;
+import org.letscareer.letscareer.domain.application.type.ApplicationStatus;
 
-@Builder
+import java.time.LocalDateTime;
+
 public record MyApplicationVo(
         Long id,
+        ApplicationStatus status,
         String programTitle,
-        String programStartDate,
-        String programEndDate
+        LocalDateTime programStartDate,
+        LocalDateTime programEndDate
 ) {
+    @Builder
+    public MyApplicationVo(Long id,
+                           Boolean paymentIsConfirmed,
+                           String programTitle,
+                           LocalDateTime programStartDate,
+                           LocalDateTime programEndDate) {
+        this(id, ApplicationStatus.of(paymentIsConfirmed, programEndDate), programTitle, programStartDate, programEndDate);
+    }
 }
