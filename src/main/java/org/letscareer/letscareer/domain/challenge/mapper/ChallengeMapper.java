c10d683ed1ce065feb6a427b4fe2355c1e415491
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-22 22:57:31 +0900

    [feat] #46 Challenge review 조회 api 구현

--- src/main/java/org/letscareer/letscareer/domain/challenge/mapper/ChallengeMapper.java
+++ src/main/java/org/letscareer/letscareer/domain/challenge/mapper/ChallengeMapper.java
@@ -1,12 +1,14 @@
 package org.letscareer.letscareer.domain.challenge.mapper;
 
 import org.letscareer.letscareer.domain.challenge.dto.response.GetChallengeDetailResponseDto;
+import org.letscareer.letscareer.domain.challenge.dto.response.GetChallengeReviewResponseDto;
 import org.letscareer.letscareer.domain.challenge.dto.response.GetChallengesResponseDto;
 import org.letscareer.letscareer.domain.challenge.vo.ChallengeDetailVo;
 import org.letscareer.letscareer.domain.challenge.vo.ChallengeProfileVo;
 import org.letscareer.letscareer.domain.classification.vo.ChallengeClassificationDetailVo;
 import org.letscareer.letscareer.domain.faq.vo.FaqDetailVo;
 import org.letscareer.letscareer.domain.price.vo.ChallengePriceDetailVo;
+import org.letscareer.letscareer.domain.review.vo.ReviewVo;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
@@ -24,5 +26,9 @@ public class ChallengeMapper {
 
     public GetChallengesResponseDto toGetChallengesResponseDto(Page<ChallengeProfileVo> challengeProfileVos) {
         return GetChallengesResponseDto.of(challengeProfileVos);
+    }
+
+    public GetChallengeReviewResponseDto toGetChallengeReviewResponseDto(Page<ReviewVo> challengeReviewVos) {
+        return GetChallengeReviewResponseDto.of(challengeReviewVos);
     }
 }
--- src/main/java/org/letscareer/letscareer/domain/challenge/service/ChallengeService.java
+++ src/main/java/org/letscareer/letscareer/domain/challenge/service/ChallengeService.java
@@ -3,6 +3,7 @@ package org.letscareer.letscareer.domain.challenge.service;
 import org.letscareer.letscareer.domain.application.dto.response.GetChallengeApplicationsResponseDto;
 import org.letscareer.letscareer.domain.challenge.dto.request.CreateChallengeRequestDto;
 import org.letscareer.letscareer.domain.challenge.dto.response.GetChallengeDetailResponseDto;
+import org.letscareer.letscareer.domain.challenge.dto.response.GetChallengeReviewResponseDto;
 import org.letscareer.letscareer.domain.challenge.dto.response.GetChallengesResponseDto;
 import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,8 @@ public interface ChallengeService {
     GetChallengeDetailResponseDto getChallengeDetail(Long challengeId);
 
     GetChallengeApplicationsResponseDto getApplications(Long challengeId, Boolean isConfirmed);
+
+    GetChallengeReviewResponseDto getReviews(Long challengeId, Pageable pageable);
 
     void createChallenge(CreateChallengeRequestDto createChallengeRequestDto);
 
