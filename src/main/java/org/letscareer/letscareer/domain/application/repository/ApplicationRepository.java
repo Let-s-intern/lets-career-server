7ac8fe74de7b4b05131e3d3304e2ef499b69cca9
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-22 21:16:39 +0900

    [feat] #46 application repository 구현

---
+++ src/main/java/org/letscareer/letscareer/domain/application/repository/ApplicationRepository.java
@@ -0,0 +1,7 @@
+package org.letscareer.letscareer.domain.application.repository;
+
+import org.letscareer.letscareer.domain.application.entity.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+}
