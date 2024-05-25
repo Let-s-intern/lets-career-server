35840b5a3f1fce786f7c938266a3493e49ae1c6a
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-26 00:35:50 +0900

    [feat] #48 기간 조건 통합 프로그램 조회 dto static 호출 메서드 구현

--- src/main/java/org/letscareer/letscareer/domain/program/mapper/ProgramMapper.java
+++ src/main/java/org/letscareer/letscareer/domain/program/mapper/ProgramMapper.java
@@ -1,9 +1,12 @@
 package org.letscareer.letscareer.domain.program.mapper;
 
 import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
+import org.letscareer.letscareer.domain.program.dto.response.GetProgramForDurationResponseDto;
 import org.letscareer.letscareer.domain.program.dto.response.GetProgramResponseDto;
+import org.letscareer.letscareer.domain.program.dto.response.GetProgramsForDurationResponseDto;
 import org.letscareer.letscareer.domain.program.dto.response.GetProgramsResponseDto;
 import org.letscareer.letscareer.domain.program.vo.AdminProgramVo;
+import org.letscareer.letscareer.domain.program.vo.ProgramForDurationVo;
 import org.letscareer.letscareer.global.common.entity.PageInfo;
 import org.springframework.stereotype.Component;
 
@@ -17,5 +20,13 @@ public class ProgramMapper {
 
     public GetProgramResponseDto<?> toGetProgramResponseDto(AdminProgramVo content, List<?> classificationList) {
         return GetProgramResponseDto.of(content, classificationList);
+    }
+
+    public GetProgramsForDurationResponseDto toGetProgramsForDurationResponseDto(List<GetProgramForDurationResponseDto<?>> programList) {
+        return GetProgramsForDurationResponseDto.of(programList);
+    }
+
+    public GetProgramForDurationResponseDto<?> toGetProgramForDurationResponseDto(ProgramForDurationVo programVo, List<?> classificationList) {
+        return GetProgramForDurationResponseDto.of(programVo, classificationList);
     }
 }
