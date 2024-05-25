1a4f9f6db96c81c4da7f593d903f3b58ccaf9ed7
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-26 00:36:04 +0900

    [feat] #48 기간 조건 통합 프로그램 조회 쿼리 구현

--- src/main/java/org/letscareer/letscareer/domain/program/repository/ProgramQueryRepository.java
+++ src/main/java/org/letscareer/letscareer/domain/program/repository/ProgramQueryRepository.java
@@ -1,9 +1,14 @@
 package org.letscareer.letscareer.domain.program.repository;
 
+import org.letscareer.letscareer.domain.program.dto.request.GetProgramsForDurationRequestDto;
 import org.letscareer.letscareer.domain.program.vo.AdminProgramVo;
+import org.letscareer.letscareer.domain.program.vo.ProgramForDurationVo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface ProgramQueryRepository {
     Page<AdminProgramVo> findAdminProgramVos(Pageable pageable);
+    List<ProgramForDurationVo>  findProgramForDurationVos(GetProgramsForDurationRequestDto requestDto);
 }
