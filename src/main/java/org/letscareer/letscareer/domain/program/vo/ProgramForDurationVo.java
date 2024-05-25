98f5d9cd91e025a55070f91da6db06f0b1239b40
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-26 00:34:24 +0900

    [feat] #48 기간 조건 통합 프로그램 조회 vo 구현

---
+++ src/main/java/org/letscareer/letscareer/domain/program/vo/ProgramForDurationVo.java
@@ -0,0 +1,20 @@
+package org.letscareer.letscareer.domain.program.vo;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.letscareer.letscareer.domain.program.type.ProgramType;
+
+import java.time.LocalDateTime;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record ProgramForDurationVo(
+        Long id,
+        ProgramType programType,
+        String title,
+        String thumbnail,
+        String shortDesc,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        LocalDateTime deadline
+) {
+}
