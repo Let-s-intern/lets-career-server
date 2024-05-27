a7724a6e59b95b05a4ea91fc373c55f920bdb539
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-27 21:51:53 +0900

    [feat] #52 유저 배너 조회 쿼리 구현

--- src/main/java/org/letscareer/letscareer/domain/banner/repository/LineBannerQueryRepositoryImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/banner/repository/LineBannerQueryRepositoryImpl.java
@@ -1,10 +1,13 @@
 package org.letscareer.letscareer.domain.banner.repository;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.letscareer.letscareer.domain.banner.vo.LineBannerAdminVo;
+import org.letscareer.letscareer.domain.banner.vo.LineBannerUserVo;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.letscareer.letscareer.domain.banner.entity.QLineBanner.lineBanner;
@@ -30,5 +33,36 @@ public class LineBannerQueryRepositoryImpl implements LineBannerQueryRepository 
                 .from(lineBanner)
                 .orderBy(lineBanner.id.desc())
                 .fetch();
+    }
+
+    @Override
+    public List<LineBannerUserVo> findAllLineBannerUserVos() {
+        return queryFactory
+                .select(Projections.constructor(LineBannerUserVo.class,
+                        lineBanner.id,
+                        lineBanner.title,
+                        lineBanner.link,
+                        lineBanner.startDate,
+                        lineBanner.endDate,
+                        lineBanner.isValid,
+                        lineBanner.contents,
+                        lineBanner.colorCode,
+                        lineBanner.textColorCode))
+                .from(lineBanner)
+                .where(
+                        isVisible(),
+                        isWithinDateRange()
+                )
+                .orderBy(lineBanner.id.desc())
+                .fetch();
+    }
+
+    private BooleanExpression isVisible() {
+        return lineBanner.isVisible.eq(true);
+    }
+
+    private BooleanExpression isWithinDateRange() {
+        LocalDateTime now = LocalDateTime.now();
+        return lineBanner.startDate.loe(now).and(lineBanner.endDate.goe(now));
     }
 }
--- src/main/java/org/letscareer/letscareer/domain/banner/repository/MainBannerQueryRepository.java
+++ src/main/java/org/letscareer/letscareer/domain/banner/repository/MainBannerQueryRepository.java
@@ -1,9 +1,12 @@
 package org.letscareer.letscareer.domain.banner.repository;
 
 import org.letscareer.letscareer.domain.banner.vo.BannerAdminVo;
+import org.letscareer.letscareer.domain.banner.vo.BannerUserVo;
 
 import java.util.List;
 
 public interface MainBannerQueryRepository {
     List<BannerAdminVo> findAllMainBannerAdminVos();
+
+    List<BannerUserVo> findAllUserBannerAdminVos();
 }
