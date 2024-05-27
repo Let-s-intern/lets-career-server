bbed7aabfeaabb3597245f76b63dba244a84a191
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-28 00:15:15 +0900

    [feat] #52 challenge 신청에 필요한 api 쿼리 구현

--- src/main/java/org/letscareer/letscareer/domain/challenge/repository/ChallengeQueryRepository.java
+++ src/main/java/org/letscareer/letscareer/domain/challenge/repository/ChallengeQueryRepository.java
@@ -1,7 +1,6 @@
 package org.letscareer.letscareer.domain.challenge.repository;
 
-import org.letscareer.letscareer.domain.challenge.vo.ChallengeDetailVo;
-import org.letscareer.letscareer.domain.challenge.vo.ChallengeProfileVo;
+import org.letscareer.letscareer.domain.challenge.vo.*;
 import org.letscareer.letscareer.domain.classification.type.ProgramClassification;
 import org.letscareer.letscareer.domain.program.type.ProgramStatusType;
 import org.springframework.data.domain.Page;
@@ -14,4 +13,10 @@ public interface ChallengeQueryRepository {
     Optional<ChallengeDetailVo> findChallengeDetailById(Long challengeId);
 
     Page<ChallengeProfileVo> findChallengeProfiles(List<ProgramClassification> typeList, List<ProgramStatusType> statusList, Pageable pageable);
+
+    Optional<ChallengeThumbnailVo> findChallengeThumbnailVo(Long challengeId);
+
+    Optional<ChallengeContentVo> findChallengeContentVo(Long challengeId);
+
+    Optional<ChallengeApplicationFormVo> findChallengeApplicationFormVo(Long challengeId);
 }
