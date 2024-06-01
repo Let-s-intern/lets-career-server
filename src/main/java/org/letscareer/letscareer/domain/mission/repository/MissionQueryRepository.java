48f67cc5ac418c52e970864ada79c604d4cf68a1
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-02 00:02:46 +0900

    [feat] #60 challenge mission 조회 쿼리 구현

--- src/main/java/org/letscareer/letscareer/domain/mission/repository/MissionQueryRepository.java
+++ src/main/java/org/letscareer/letscareer/domain/mission/repository/MissionQueryRepository.java
@@ -1,4 +1,9 @@
 package org.letscareer.letscareer.domain.mission.repository;
 
+import org.letscareer.letscareer.domain.mission.vo.MissionForChallengeVo;
+
+import java.util.List;
+
 public interface MissionQueryRepository {
+    List<MissionForChallengeVo> findMissionForChallengeVos(Long challengeId);
 }
--- src/main/java/org/letscareer/letscareer/domain/mission/repository/MissionQueryRepositoryImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/mission/repository/MissionQueryRepositoryImpl.java
@@ -1,4 +1,42 @@
 package org.letscareer.letscareer.domain.mission.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.letscareer.letscareer.domain.mission.vo.MissionForChallengeVo;
+
+import java.util.List;
+
+import static org.letscareer.letscareer.domain.challenge.entity.QChallenge.challenge;
+import static org.letscareer.letscareer.domain.mission.entity.QMission.mission;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MissionQueryRepositoryImpl implements MissionQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<MissionForChallengeVo> findMissionForChallengeVos(Long challengeId) {
+        return queryFactory
+                .select(Projections.constructor(MissionForChallengeVo.class,
+                        mission.id,
+                        mission.missionStatusType,
+                        mission.attendanceCount,
+                        mission.lateAttendanceCount,
+                        mission.startDate,
+                        mission.endDate
+                ))
+                .from(mission)
+                .leftJoin(mission.challenge, challenge)
+                .where(
+                        eqChallengeId(challengeId)
+                )
+                .orderBy(challenge.startDate.asc())
+                .fetch();
+    }
+
+    private BooleanExpression eqChallengeId(Long challengeId) {
+        return challengeId != null ? challenge.id.eq(challengeId) : null;
+    }
 }
