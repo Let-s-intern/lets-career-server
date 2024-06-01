0663d8967d1587157f859d72ba08cfc79beb6772
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-02 00:02:25 +0900

    [feat] #60 challenge mission 조회 쿼리 호출 메서드 구현

--- src/main/java/org/letscareer/letscareer/domain/mission/helper/MissionHelper.java
+++ src/main/java/org/letscareer/letscareer/domain/mission/helper/MissionHelper.java
@@ -3,12 +3,19 @@ package org.letscareer.letscareer.domain.mission.helper;
 import lombok.RequiredArgsConstructor;
 import org.letscareer.letscareer.domain.mission.entity.Mission;
 import org.letscareer.letscareer.domain.mission.repository.MissionRepository;
+import org.letscareer.letscareer.domain.mission.vo.MissionForChallengeVo;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class MissionHelper {
     private final MissionRepository missionRepository;
+
+    public List<MissionForChallengeVo> findMissionForChallengeVos(Long challengeId) {
+        return missionRepository.findMissionForChallengeVos(challengeId);
+    }
 
     public void saveMission(Mission mission) {
         missionRepository.save(mission);
