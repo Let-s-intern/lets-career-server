c1c9bf9807a8eec688f4e39ed8481ac1afceaaca
seoyamin <seoyamin@ewhain.net>
2024-06-02 13:39:17 +0900

    [chore] #61 CreatePaymentRequestDto finalPrice 제거

--- src/main/java/org/letscareer/letscareer/domain/payment/dto/request/CreatePaymentRequestDto.java
+++ src/main/java/org/letscareer/letscareer/domain/payment/dto/request/CreatePaymentRequestDto.java
@@ -1,7 +1,6 @@
 package org.letscareer.letscareer.domain.payment.dto.request;
 
 public record CreatePaymentRequestDto(
-        Integer finalPrice,
         Long couponId,
         Long priceId
 ) {
