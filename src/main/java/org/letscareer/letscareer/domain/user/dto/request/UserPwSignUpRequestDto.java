3d47ea082486bf8353d1fd79fb2c7d15c02283d4
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-17 00:58:49 +0900

    [fix] #67 회원가입시 유입 경로 추가

--- src/main/java/org/letscareer/letscareer/domain/user/dto/request/UserPwSignUpRequestDto.java
+++ src/main/java/org/letscareer/letscareer/domain/user/dto/request/UserPwSignUpRequestDto.java
@@ -8,6 +8,7 @@ public record UserPwSignUpRequestDto(
         @NotEmpty(message = "이름을 입력해주세요") String name,
         @NotEmpty(message = "전화번호를 입력해주세요") String phoneNum,
         @NotEmpty(message = "비밀번호를 입력해주세요") String password,
+        @NotEmpty(message = "유입경로를 입력해주세요") String inflowPath,
         @NotNull(message = "마켓팅 동의 정보를 입력해주세요") Boolean marketingAgree
 ) {
 }
