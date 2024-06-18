c73e6388a4f8a12c3574c3be6e59e061b22ed98d
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-18 11:36:32 +0900

    [feat] #67 swagger default 서버 url 추가

--- src/main/java/org/letscareer/letscareer/LetsCareerApplication.java
+++ src/main/java/org/letscareer/letscareer/LetsCareerApplication.java
@@ -1,11 +1,14 @@
 package org.letscareer.letscareer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://www.letscareer-test.shop", description = "Default Server url")})
 @EnableScheduling
+@SpringBootApplication
 public class LetsCareerApplication {
 
     public static void main(String[] args) {
