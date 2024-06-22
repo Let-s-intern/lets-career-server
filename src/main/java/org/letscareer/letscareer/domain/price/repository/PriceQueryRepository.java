7ef116daf5a193ab5f77c2e6684502800a780134
seoyamin <seoyamin@ewhain.net>
2024-06-22 19:18:30 +0900

    [feat] #70 마이페이지 결제 내역

--- src/main/java/org/letscareer/letscareer/domain/price/repository/PriceQueryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.letscareer.letscareer.domain.price.repository;
-
-import org.letscareer.letscareer.domain.price.vo.PriceDetailVo;
-
-import java.util.Optional;
-
-public interface PriceQueryRepository {
-    Optional<PriceDetailVo> findPriceDetailVoByPriceId(Long priceId);
-}
