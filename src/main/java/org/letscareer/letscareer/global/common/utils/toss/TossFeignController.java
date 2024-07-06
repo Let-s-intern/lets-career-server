fddabf51060edf99f44b7e1494a38d7dfb485276
RyuKwanKon <rhks33rhs@gmail.com>
2024-07-06 15:31:10 +0900

    LC-122-feat toss payments feign 구현

--- src/main/java/org/letscareer/letscareer/global/common/utils/toss/TossFeignController.java
+++ src/main/java/org/letscareer/letscareer/global/common/utils/toss/TossFeignController.java
@@ -1,11 +1,14 @@
 package org.letscareer.letscareer.global.common.utils.toss;
 
+import org.letscareer.letscareer.domain.pg.dto.request.TossPaymentsRequestDto;
+import org.letscareer.letscareer.domain.pg.dto.response.TossPaymentsResponseDto;
 import org.letscareer.letscareer.global.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "toss-feign-client", url = "https://api.tosspayments.com/v1/payments", configuration = FeignClientConfig.class)
 public interface TossFeignController {
     @PostMapping("/confirm")
-    String createPayments();
+    TossPaymentsResponseDto createPayments(@RequestBody final TossPaymentsRequestDto requestDto);
 }
