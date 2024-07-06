f90b190ba995f5de271e7cde3750d056e007d2b4
RyuKwanKon <rhks33rhs@gmail.com>
2024-07-06 15:28:14 +0900

    LC-122-refactor factory 패턴 config 파일 위치 이동 & PgProvider 적용

---
+++ src/main/java/org/letscareer/letscareer/global/config/FactoryConfig.java
@@ -0,0 +1,24 @@
+package org.letscareer.letscareer.global.config;
+
+import org.letscareer.letscareer.domain.application.service.ApplicationServiceFactory;
+import org.letscareer.letscareer.domain.pg.provider.PgProviderFactory;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FactoryConfig {
+    @Bean
+    public ServiceLocatorFactoryBean applicationServiceFactoryBean() {
+        ServiceLocatorFactoryBean factoryBean = new ServiceLocatorFactoryBean();
+        factoryBean.setServiceLocatorInterface(ApplicationServiceFactory.class);
+        return factoryBean;
+    }
+
+    @Bean
+    public ServiceLocatorFactoryBean pgProviderFactoryBean() {
+        ServiceLocatorFactoryBean factoryBean = new ServiceLocatorFactoryBean();
+        factoryBean.setServiceLocatorInterface(PgProviderFactory.class);
+        return factoryBean;
+    }
+}
