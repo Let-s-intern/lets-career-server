bff1fbf523ecbf67d2546ffa8f8d8f7722c25447
RyuKwanKon <rhks33rhs@gmail.com>
2024-07-06 13:41:20 +0900

    LC-122-feat feign implementation 위치 지정

--- src/main/java/org/letscareer/letscareer/global/config/FeignClientConfig.java
+++ src/main/java/org/letscareer/letscareer/global/config/FeignClientConfig.java
@@ -1,10 +1,9 @@
 package org.letscareer.letscareer.global.config;
 
-import org.letscareer.letscareer.LetsCareerApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
-@EnableFeignClients(basePackageClasses = LetsCareerApplication.class)
+@EnableFeignClients("org.letscareer.letscareer.global.common.utils.toss")
 @Configuration
 public class FeignClientConfig {
 }
