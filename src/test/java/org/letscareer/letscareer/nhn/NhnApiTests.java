48b32f1e27a963aee4aa6c77b4cc72524d9e3f34
RyuKwanKon <rhks33rhs@gmail.com>
2024-07-13 14:10:56 +0900

    LC-176-feat 결제 승인 템플릿 테슽 코드 작성

--- src/test/java/org/letscareer/letscareer/nhn/NhnApiTests.java
+++ src/test/java/org/letscareer/letscareer/nhn/NhnApiTests.java
@@ -2,11 +2,17 @@ package org.letscareer.letscareer.nhn;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.letscareer.letscareer.domain.nhn.dto.request.CreateMessageRequestDto;
+import org.letscareer.letscareer.domain.nhn.dto.request.CreditConfirmParameter;
+import org.letscareer.letscareer.domain.nhn.dto.request.RecipientInfo;
 import org.letscareer.letscareer.domain.nhn.dto.response.CreateMessageResponseDto;
 import org.letscareer.letscareer.global.common.utils.nhn.NhnFeignController;
 import org.letscareer.letscareer.global.common.utils.nhn.NhnSecretKeyReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,18 +32,25 @@ public class NhnApiTests {
         String appKey = nhnSecretKeyReader.getAppKey();
 
         // then
-        System.out.println(appKey);
         assertThat(appKey).isNotEmpty();
     }
 
     @Test
-    @DisplayName("메시지 치환 발송 요청 api")
+    @DisplayName("결제 확인 메시지 치환 발송 요청 api")
     void sendMessageToKakao() {
         // given
+        String senderKey = nhnSecretKeyReader.getSendKey();
+        String templateKey = nhnSecretKeyReader.getTemplateCode();
         String appKey = nhnSecretKeyReader.getAppKey();
+        String recipientNo = "010-9278-7357";
+        CreditConfirmParameter creditConfirmParameter = CreditConfirmParameter.of("류관곤", "101", "프로그램이지롱", "1,000,000,000");
 
         // when
-        CreateMessageResponseDto responseDto = nhnFeignController.createMessage(appKey, null);
+        RecipientInfo<?> recipientInfo = RecipientInfo.of(recipientNo, creditConfirmParameter);
+        List<RecipientInfo<?>> recipientInfoList = new ArrayList<>();
+        recipientInfoList.add(recipientInfo);
+        CreateMessageRequestDto requestDto = CreateMessageRequestDto.of(senderKey, templateKey, recipientInfoList);
+        CreateMessageResponseDto responseDto = nhnFeignController.createMessage(appKey, requestDto);
 
         // then
         System.out.println(responseDto);
