090f3ae75896727e40431643e4f8c4cd8456d2a5
seoyamin <seoyamin@ewhain.net>
2024-07-13 13:00:16 +0900

    LC-161-feat ApplicationWithdrawalRecord entity 삭제

--- src/main/java/org/letscareer/letscareer/domain/withdraw/helper/WithdrawHelper.java
+++ src/main/java/org/letscareer/letscareer/domain/withdraw/helper/WithdrawHelper.java
@@ -2,11 +2,8 @@ package org.letscareer.letscareer.domain.withdraw.helper;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.letscareer.letscareer.domain.program.type.ProgramType;
 import org.letscareer.letscareer.domain.user.entity.User;
-import org.letscareer.letscareer.domain.withdraw.entity.ApplicationWithdrawalRecord;
 import org.letscareer.letscareer.domain.withdraw.entity.UserWithdrawalRecord;
-import org.letscareer.letscareer.domain.withdraw.repository.ApplicationWithdrawalRecordRepository;
 import org.letscareer.letscareer.domain.withdraw.repository.UserWithdrawalRecordRepository;
 import org.springframework.stereotype.Component;
 
@@ -14,15 +11,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class WithdrawHelper {
     private final UserWithdrawalRecordRepository userWithdrawalRecordRepository;
-    private final ApplicationWithdrawalRecordRepository applicationWithdrawalRecordRepository;
 
     public void createUserWithdrawalRecordAndSave(User user) {
         UserWithdrawalRecord withdrawalRecord = UserWithdrawalRecord.createUserWithdrawalRecord(user);
         userWithdrawalRecordRepository.save(withdrawalRecord);
-    }
-
-    public void createApplicationWithdrawalRecordAndSave(Long programId, String title, ProgramType programType, User user) {
-        ApplicationWithdrawalRecord withdrawalRecord = ApplicationWithdrawalRecord.createApplicationWithdrawalRecord(programId, title, programType, user);
-        applicationWithdrawalRecordRepository.save(withdrawalRecord);
     }
 }
