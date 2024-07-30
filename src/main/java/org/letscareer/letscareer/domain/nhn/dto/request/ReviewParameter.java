52d9e582cd7f6279958a8d1809280fb48722e127
seoyamin <seoyamin@ewhain.net>
2024-07-30 11:26:02 +0900

    LC-414-feat 후기 요청 알림톡 템플릿 설정

--- src/main/java/org/letscareer/letscareer/domain/nhn/dto/request/ReviewParameter.java
+++ src/main/java/org/letscareer/letscareer/domain/nhn/dto/request/ReviewParameter.java
@@ -2,14 +2,19 @@ package org.letscareer.letscareer.domain.nhn.dto.request;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import org.letscareer.letscareer.domain.program.type.ProgramType;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record ReviewParameter(
-        String userName
+        String userName,
+        String program,
+        Long programId
 ) {
-    public static ReviewParameter of(String userName) {
+    public static ReviewParameter of(String userName, ProgramType programType, Long programId) {
         return ReviewParameter.builder()
                 .userName(userName)
+                .program(programType.getDesc())
+                .programId(programId)
                 .build();
     }
 }
