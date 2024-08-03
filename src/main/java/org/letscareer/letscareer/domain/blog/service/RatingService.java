34bc633b3a8e6e74d77fd6afa7c1b42d338f8410
RyuKwanKon <rhks33rhs@gmail.com>
2024-08-03 16:55:13 +0900

    LC-453-feat 블로그 후기 전체 조회 api 개발

--- src/main/java/org/letscareer/letscareer/domain/blog/service/RatingService.java
+++ src/main/java/org/letscareer/letscareer/domain/blog/service/RatingService.java
@@ -1,11 +1,14 @@
 package org.letscareer.letscareer.domain.blog.service;
 
 import org.letscareer.letscareer.domain.blog.dto.request.CreateRatingRequestDto;
+import org.letscareer.letscareer.domain.blog.dto.response.rating.GetBlogRatingsResponseDto;
 import org.letscareer.letscareer.domain.blog.dto.response.rating.GetRatingsResponseDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface RatingService {
-    GetRatingsResponseDto getBlogRatings(Long blogId);
+    GetRatingsResponseDto getRatings(Pageable pageable);
+    GetBlogRatingsResponseDto getBlogRatings(Long blogId);
     void createBlogRating(Long blogId, CreateRatingRequestDto requestDto);
 }
