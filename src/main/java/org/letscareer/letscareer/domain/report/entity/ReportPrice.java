a600f2a5ab220b508150235a0ffd33d3777cedbe
seoyamin <seoyamin@ewhain.net>
2024-08-24 12:38:30 +0900

    LC-584-feat ReportPrice 엔티티 생성

---
+++ src/main/java/org/letscareer/letscareer/domain/report/entity/ReportPrice.java
@@ -0,0 +1,26 @@
+package org.letscareer.letscareer.domain.report.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.letscareer.letscareer.global.common.entity.BaseTimeEntity;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "report_price")
+@Entity
+public class ReportPrice extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_price_id")
+    private Long id;
+
+    private Integer price;
+
+    private Integer discountPrice;
+
+    private Integer correctionPrice;
+
+    private Integer correctionDiscountPrice;
+}
