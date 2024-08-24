3a2a8bb1822b3d52755eb0e1ba2d3bd1a6329cf4
seoyamin <seoyamin@ewhain.net>
2024-08-24 13:30:44 +0900

    LC-584-feat ReportFeedbackApplication 엔티티 생성

---
+++ src/main/java/org/letscareer/letscareer/domain/report/type/ReportDesiredDateType.java
@@ -0,0 +1,18 @@
+package org.letscareer.letscareer.domain.report.type;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.letscareer.letscareer.global.common.utils.enm.EnumField;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum ReportDesiredDateType implements EnumField {
+    DESIRED_DATE_1(1, "1"),
+    DESIRED_DATE_2(2, "2"),
+    DESIRED_DATE_3(3, "3"),
+    DESIRED_DATE_ADMIN(4, "어드민");
+
+    private final Integer code;
+    private final String desc;
+}
