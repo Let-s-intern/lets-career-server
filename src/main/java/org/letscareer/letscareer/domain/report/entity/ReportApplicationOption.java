fcfb3afd6287c8cff6d424d1f86107ded7325012
seoyamin <seoyamin@ewhain.net>
2024-08-24 13:21:36 +0900

    LC-584-feat ReportApplicationOption 엔티티 생성

---
+++ src/main/java/org/letscareer/letscareer/domain/report/entity/ReportApplicationOption.java
@@ -0,0 +1,28 @@
+package org.letscareer.letscareer.domain.report.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.letscareer.letscareer.global.common.entity.BaseTimeEntity;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("report_application_option")
+@Getter
+@Entity
+public class ReportApplicationOption extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_application_option_id")
+    private Long id;
+
+    private Integer price;
+
+    private Integer discountPrice;
+
+    private String title;
+
+    private String code;
+}
