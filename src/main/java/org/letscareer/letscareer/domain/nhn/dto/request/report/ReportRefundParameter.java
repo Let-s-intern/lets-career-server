e46282f61d2ca33e17700b23deb99248b5bfca46
seoyamin <seoyamin@ewhain.net>
2024-09-04 16:25:50 +0900

    LC-593-feat 서류 진단 신청서 환불 알림톡 발송

--- src/main/java/org/letscareer/letscareer/domain/nhn/dto/request/report/ReportRefundParameter.java
+++ src/main/java/org/letscareer/letscareer/domain/nhn/dto/request/report/ReportRefundParameter.java
@@ -2,6 +2,7 @@ package org.letscareer.letscareer.domain.nhn.dto.request.report;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import org.letscareer.letscareer.domain.payment.type.RefundType;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record ReportRefundParameter(
@@ -10,24 +11,21 @@ public record ReportRefundParameter(
         String reportTitle,
         String orderStatus,
         Integer paymentPrice,
-        Integer refundPrice,
-        Long applicationId
+        Integer refundPrice
 ) {
     public static ReportRefundParameter of(String name,
                                            String orderId,
                                            String reportTitle,
-                                           String orderStatus,
+                                           RefundType refundType,
                                            Integer paymentPrice,
-                                           Integer refundPrice,
-                                           Long applicationId) {
+                                           Integer refundPrice) {
         return ReportRefundParameter.builder()
                 .name(name)
                 .orderId(orderId)
                 .reportTitle(reportTitle)
-                .orderStatus(orderStatus)
+                .orderStatus(refundType.equals(RefundType.ALL) ? "전체취소" : "부분취소")
                 .paymentPrice(paymentPrice)
                 .refundPrice(refundPrice)
-                .applicationId(applicationId)
                 .build();
     }
 }
--- src/test/java/org/letscareer/letscareer/nhn/ReportNhnApiTests.java
+++ src/test/java/org/letscareer/letscareer/nhn/ReportNhnApiTests.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.letscareer.letscareer.domain.application.entity.report.ReportApplication;
 import org.letscareer.letscareer.domain.application.helper.ReportApplicationHelper;
+import org.letscareer.letscareer.domain.nhn.dto.request.report.ReportRefundParameter;
 import org.letscareer.letscareer.domain.report.dto.req.UpdateReportDocumentRequestDto;
+import org.letscareer.letscareer.domain.report.service.impl.CancelReportApplicationServiceImpl;
 import org.letscareer.letscareer.domain.report.service.impl.UpdateReportDocumentServiceImpl;
+import org.letscareer.letscareer.domain.user.entity.User;
+import org.letscareer.letscareer.domain.user.helper.UserHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,9 +19,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ReportNhnApiTests {
     @Autowired
     private UpdateReportDocumentServiceImpl updateReportDocumentService;
-
+    @Autowired
+    private CancelReportApplicationServiceImpl cancelReportApplicationService;
     @Autowired
     private ReportApplicationHelper reportApplicationHelper;
+    @Autowired
+    private UserHelper userHelper;
 
     @Test
     @DisplayName("진단서 진단완료 알림톡")
@@ -32,5 +39,20 @@ public class ReportNhnApiTests {
         // then
         ReportApplication reportApplication = reportApplicationHelper.findReportApplicationByReportApplicationIdOrThrow(applicationId);
         assertThat(reportApplication.getReportUrl()).isEqualTo("https://www.google.com");
+    }
+
+    @Test
+    @DisplayName("서류 진단 신청서 환불 알림톡")
+    void reportRefundNotification() {
+        // given
+        User user = userHelper.findUserByIdOrThrow(26L);
+        Long applicationId = 262L;
+
+        // when
+        cancelReportApplicationService.execute(user, applicationId);
+
+        // then
+        ReportApplication reportApplication = reportApplicationHelper.findReportApplicationByReportApplicationIdOrThrow(applicationId);
+        assertThat(reportApplication.getIsCanceled()).isEqualTo(true);
     }
 }
