8408f698f8846e1c7e0c6a797627e5aaceecbc0a
RyuKwanKon <rhks33rhs@gmail.com>
2024-09-04 20:34:18 +0900

    LC-605-fix optionIds empty값 처리

--- src/main/java/org/letscareer/letscareer/domain/report/helper/ReportOptionHelper.java
+++ src/main/java/org/letscareer/letscareer/domain/report/helper/ReportOptionHelper.java
@@ -7,7 +7,9 @@ import org.letscareer.letscareer.domain.report.entity.ReportOption;
 import org.letscareer.letscareer.domain.report.repository.ReportOptionRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
@@ -20,6 +22,7 @@ public class ReportOptionHelper {
     }
 
     public List<ReportOption> findReportOptionsByReportIdAndOptionIds(Long reportId, List<Long> optionIds) {
+        if (Objects.isNull(optionIds) || optionIds.isEmpty()) return new ArrayList<>();
         return reportOptionRepository.findReportOptionsByReportIdAndOptionIds(reportId, optionIds);
     }
 
