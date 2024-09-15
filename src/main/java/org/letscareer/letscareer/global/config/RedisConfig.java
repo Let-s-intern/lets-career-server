e0b7f93cdf4a9c7cf4dd7ef382f6613f91f7d47f
seoyamin <seoyamin@ewhain.net>
2024-09-15 17:09:49 +0900

    LC-640-fix Redis Expire Notification 코드 주석 처리

--- src/main/java/org/letscareer/letscareer/global/config/RedisConfig.java
+++ src/main/java/org/letscareer/letscareer/global/config/RedisConfig.java
@@ -1,27 +1,28 @@
 package org.letscareer.letscareer.global.config;
 
-import org.letscareer.letscareer.global.common.utils.redis.listener.EventListener;
+//import org.letscareer.letscareer.global.common.utils.redis.listener.EventListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
+//import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+//import org.springframework.data.redis.listener.PatternTopic;
+//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories(
-        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP,
-        keyspaceNotificationsConfigParameter = ""
-)
+@EnableRedisRepositories
+//@EnableRedisRepositories(
+//        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP,
+//        keyspaceNotificationsConfigParameter = ""
+//)
 public class RedisConfig {
-    private final String EXPIRED_EVENT_PATTERN = "__keyevent@*__:expired";
-    private final String EXPIRED_SPACE_PATTERN = "__keyspace@*__:expired";
-    private final String SET_EVENT_PATTERN = "__keyevent@*__:set";
+//    private final String EXPIRED_EVENT_PATTERN = "__keyevent@*__:expired";
+//    private final String EXPIRED_SPACE_PATTERN = "__keyspace@*__:expired";
+//    private final String SET_EVENT_PATTERN = "__keyevent@*__:set";
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -42,14 +43,14 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory,
-                                                                       EventListener eventListener) {
-        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
-        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
-        redisMessageListenerContainer.addMessageListener(eventListener, new PatternTopic(SET_EVENT_PATTERN));
-        redisMessageListenerContainer.addMessageListener(eventListener, new PatternTopic(EXPIRED_EVENT_PATTERN));
-        redisMessageListenerContainer.addMessageListener(eventListener, new PatternTopic(EXPIRED_SPACE_PATTERN));
-        return redisMessageListenerContainer;
-    }
+//    @Bean
+//    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory,
+//                                                                       EventListener eventListener) {
+//        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
+//        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
+//        redisMessageListenerContainer.addMessageListener(eventListener, new PatternTopic(SET_EVENT_PATTERN));
+//        redisMessageListenerContainer.addMessageListener(eventListener, new PatternTopic(EXPIRED_EVENT_PATTERN));
+//        redisMessageListenerContainer.addMessageListener(eventListener, new PatternTopic(EXPIRED_SPACE_PATTERN));
+//        return redisMessageListenerContainer;
+//    }
 }
