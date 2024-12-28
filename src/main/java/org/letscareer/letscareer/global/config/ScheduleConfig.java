5d191bba1c5250b81cc2f0e443b912ad95fe1507
seoyamin <seoyamin@ewhain.net>
2024-12-28 17:20:12 +0900

    LC-1046-chore taskScheduler setPoolSize 3

--- src/main/java/org/letscareer/letscareer/global/config/ScheduleConfig.java
+++ src/main/java/org/letscareer/letscareer/global/config/ScheduleConfig.java
@@ -6,7 +6,9 @@ import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import javax.sql.DataSource;
 
@@ -14,6 +16,14 @@ import javax.sql.DataSource;
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "10m")
 public class ScheduleConfig {
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(3);
+        scheduler.setThreadNamePrefix("scheduled-task-");
+        return scheduler;
+    }
+
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
         return new JdbcTemplateLockProvider(
