18d4ac2d28536382bcb744219c0eda7018cabb2d
seoyamin <seoyamin@ewhain.net>
2024-12-29 03:36:03 +0900

    LC-1046-chore fixedRate -> 10분 단위 cron으로 변경

--- src/main/java/org/letscareer/letscareer/global/batch/scheduler/ReportNotificationScheduler.java
+++ src/main/java/org/letscareer/letscareer/global/batch/scheduler/ReportNotificationScheduler.java
@@ -47,8 +47,10 @@ public class ReportNotificationScheduler {
         }
     }
 
-    @Scheduled(fixedRate = 60000)
-    @SchedulerLock(name = "reportIngNotificationJob", lockAtMostFor = "59s", lockAtLeastFor = "59s")
+//    @Scheduled(fixedRate = 60000)
+//    @SchedulerLock(name = "reportIngNotificationJob", lockAtMostFor = "59s", lockAtLeastFor = "59s")
+    @Scheduled(cron = "0 30,10,20,30,40,50 * * * *")
+    @SchedulerLock(name = "reportIngNotificationJob", lockAtMostFor = "9m", lockAtLeastFor = "8m30s")
     public void sendReportIngNotification() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
         List<Long> reportApplicationList = reportApplicationHelper.findIngNotificationReportApplicationIds();
         for(Long reportApplicationId : reportApplicationList) {
