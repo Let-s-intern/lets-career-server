d8d70420e41e94d10bf9c134162f5be7ab85c961
seoyamin <seoyamin@ewhain.net>
2025-01-19 14:49:14 +0900

    LC-1376-feat Review 엔티티 속성 정리

--- src/main/java/org/letscareer/letscareer/domain/review/entity/Review.java
+++ src/main/java/org/letscareer/letscareer/domain/review/entity/Review.java
@@ -3,13 +3,7 @@ package org.letscareer.letscareer.domain.review.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.letscareer.letscareer.domain.application.entity.Application;
-import org.letscareer.letscareer.domain.program.type.ProgramType;
-import org.letscareer.letscareer.domain.program.type.converter.ProgramTypeConverter;
-import org.letscareer.letscareer.domain.review.dto.request.CreateOldReviewRequestDto;
-import org.letscareer.letscareer.domain.review.dto.request.UpdateOldReviewRequestDto;
 import org.letscareer.letscareer.global.common.entity.BaseTimeEntity;
-
-import static org.letscareer.letscareer.global.common.utils.entity.EntityUpdateValueUtils.updateValue;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,59 +15,14 @@ public class Review extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "review_id")
     private Long id;
-    private Integer nps;
-    private String npsAns;
-    private Boolean npsCheckAns;
-    private String content;
     private Integer score;
-    @Convert(converter = ProgramTypeConverter.class)
-    private ProgramType programType;
-    private Long programId;
+    private Integer npsScore;
+    private String goodPoint;
+    private String badPoint;
     @Builder.Default
     private Boolean isVisible = false;
-    private String programDetail;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "application_id")
     private Application application;
-
-    public static Review createReview(Application application, CreateOldReviewRequestDto reviewRequestDto) {
-        Review review = Review.builder()
-                .nps(reviewRequestDto.nps())
-                .npsAns(reviewRequestDto.npsAns())
-                .npsCheckAns(reviewRequestDto.npsCheckAns())
-                .content(reviewRequestDto.content())
-                .score(reviewRequestDto.score())
-                .programDetail(reviewRequestDto.programDetail())
-                .application(application)
-                .build();
-        return review;
-    }
-
-    public static Review createReviewByLink(Long programId, ProgramType programType, CreateOldReviewRequestDto reviewRequestDto) {
-        Review review = Review.builder()
-                .nps(reviewRequestDto.nps())
-                .npsAns(reviewRequestDto.npsAns())
-                .npsCheckAns(reviewRequestDto.npsCheckAns())
-                .content(reviewRequestDto.content())
-                .score(reviewRequestDto.score())
-                .programDetail(reviewRequestDto.programDetail())
-                .programId(programId)
-                .programType(programType)
-                .build();
-        return review;
-    }
-
-    public void updateReview(UpdateOldReviewRequestDto requestDto) {
-        this.nps = updateValue(this.nps, requestDto.nps());
-        this.npsAns = updateValue(this.npsAns, requestDto.npsAns());
-        this.npsCheckAns = updateValue(this.npsCheckAns, requestDto.npsCheckAns());
-        this.content = updateValue(this.content, requestDto.content());
-        this.score = updateValue(this.score, requestDto.score());
-        this.programDetail = updateValue(this.programDetail, requestDto.programDetail());
-    }
-
-    public void updateIsVisibleStatus(Boolean isVisible) {
-        this.isVisible = updateValue(this.isVisible, isVisible);
-    }
 }
