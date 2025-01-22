69ddcd90d2666d4e7aa3b69cc6419ac146f50447
Taeyoung Kim <lucy_ia@ewhain.net>
2025-01-23 00:26:24 +0900

    LC-1401-feat 챌/라 리뷰 요청 알림톡 수정
    - 라이브도 동일하게 oldReview -> review로 연결 변경

--- src/main/java/org/letscareer/letscareer/domain/application/repository/LiveApplicationQueryRepositoryImpl.java
+++ src/main/java/org/letscareer/letscareer/domain/application/repository/LiveApplicationQueryRepositoryImpl.java
@@ -163,7 +163,7 @@ public class LiveApplicationQueryRepositoryImpl implements LiveApplicationQueryR
     }
 
     private BooleanExpression reviewIsNull() {
-        return liveApplication._super.oldReview.isNull();
+        return liveApplication._super.review.isNull();
     }
 
     private BooleanExpression eqApplicationId(Long applicationId) {
