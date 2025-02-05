7387dcf89ebaed0687631acd137a32f1d06f3542
kimeho <kimeho0908@gmail.com>
2025-02-05 22:50:31 +0900

    LC-1401-hotfix 챌린지 알림톡 템플릿 변경적용 및 파라미터 최적화

--- src/main/java/org/letscareer/letscareer/domain/nhn/dto/request/challenge/OTRemindParameter.java
+++ src/main/java/org/letscareer/letscareer/domain/nhn/dto/request/challenge/OTRemindParameter.java
@@ -12,26 +12,24 @@ public record OTRemindParameter(
         String userName,
         String programTitle,
         @JsonFormat(pattern = "yyyy년 MM월 dd일 HH:mm", timezone = "Asia/Seoul")
-        LocalDateTime programStartDate,
-        String zoomLink,
-        String chatLink,
-        Long programId,
-        @JsonFormat(pattern = "yyyy년 MM월 dd일 HH:mm", timezone = "Asia/Seoul")
         LocalDateTime programOtStart,
         @JsonFormat(pattern = "yyyy년 MM월 dd일 HH:mm", timezone = "Asia/Seoul")
-        LocalDateTime programOtEnd
+        LocalDateTime programOtEnd,
+        String chatLink,
+        String zoomLink,
+        Long programId
+
 ) {
     public static OTRemindParameter of(String userName,
                                        Challenge challenge) {
         return OTRemindParameter.builder()
                 .userName(userName)
                 .programTitle(challenge.getTitle())
-                .programStartDate(challenge.getStartDate())
-                .zoomLink(challenge.getZoomLink().substring(8))
-                .chatLink(challenge.getChatLink())
-                .programId(challenge.getId())
                 .programOtStart(challenge.getStartDate())
                 .programOtEnd(challenge.getStartDate().plusMinutes(40))
+                .chatLink(challenge.getChatLink())
+                .zoomLink(challenge.getZoomLink().substring(8))
+                .programId(challenge.getId())
                 .build();
     }
 }
