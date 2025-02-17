eca3943db284a5bf44de55cbb9dd9961516c8364
seoyamin <seoyamin@ewhain.net>
2025-02-17 09:44:52 +0900

    LC-1639-feat Curation Controller 생성

---
+++ src/main/java/org/letscareer/letscareer/domain/curation/controller/CurationV1AdminController.java
@@ -0,0 +1,12 @@
+package org.letscareer.letscareer.domain.curation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin/curation")
+@RestController
+public class CurationV1AdminController {
+
+}
