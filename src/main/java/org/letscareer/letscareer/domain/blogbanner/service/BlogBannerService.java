e3961da8720c08e7188270144f37d79630da44dd
kimeho <kimeho0908@gmail.com>
2025-02-19 14:35:45 +0900

    LC-1646-feat BlogBanner Service 생성

---
+++ src/main/java/org/letscareer/letscareer/domain/blogbanner/service/BlogBannerService.java
@@ -0,0 +1,4 @@
+package org.letscareer.letscareer.domain.blogbanner.service;
+
+public interface BlogBannerService {
+}
---
+++ src/main/java/org/letscareer/letscareer/domain/blogbanner/service/BlogBannerServiceImpl.java
@@ -0,0 +1,11 @@
+package org.letscareer.letscareer.domain.blogbanner.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class BlogBannerServiceImpl implements BlogBannerService{
+}
