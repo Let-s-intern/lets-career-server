8add8e7bf343a1d70c6a90e6d2a381dd0fb1d7d2
seoyamin <hyeminseo310@gmail.com>
2025-04-28 16:49:39 +0900

    LC-1949-chore PriceDetailVo 생성자 필드 누락 해결

--- src/main/java/org/letscareer/letscareer/domain/price/vo/PriceDetailVo.java
+++ src/main/java/org/letscareer/letscareer/domain/price/vo/PriceDetailVo.java
@@ -1,9 +1,6 @@
 package org.letscareer.letscareer.domain.price.vo;
 
 import lombok.Builder;
-import org.letscareer.letscareer.domain.price.type.AccountType;
-
-import java.time.LocalDateTime;
 
 @Builder
 public record PriceDetailVo(
