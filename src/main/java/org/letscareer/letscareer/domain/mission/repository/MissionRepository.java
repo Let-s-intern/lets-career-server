4e6eb34a90a5e65f22505285b37f0589d3e85506
Gdm0714 <ehdals45454@naver.com>
2025-07-30 21:34:14 +0900

    0회차 미션이 존재할 때만 완료 체크하도록 수정
    - MissionRepository에 existsByChallengeIdAndTh 메소드 추가
    - MissionHelper에 hasOTMission 메소드 추가
    - ChallengeServiceImpl에서 0회차 미션 존재 여부 확인 후 완료 체크 수행
    - 챌린지에 0회차 미션이 없는 경우 1~10회차 접근 제한하지 않음

--- src/main/java/org/letscareer/letscareer/domain/mission/repository/MissionRepository.java
+++ src/main/java/org/letscareer/letscareer/domain/mission/repository/MissionRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissionRepository extends JpaRepository<Mission, Long>, MissionQueryRepository {
     void deleteAllByChallengeId(Long challengeId);
+    boolean existsByChallengeIdAndTh(Long challengeId, Integer th);
 }
